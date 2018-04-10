0de53d431f3af6c4a48fb466968a8706939cb355
beny <jlbeny100@gmail.com>
2018-04-11 01:09:27 +0200

    @WebServlet("/*.do") -> @WebServlet("*.do")

--- src/com/valoyes/patterns/frontcontroller/FrontController.java
+++ src/com/valoyes/patterns/frontcontroller/FrontController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
  * @author benito
  *
  */
-@WebServlet("/*.do")
+@WebServlet("*.do")
 public class FrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
