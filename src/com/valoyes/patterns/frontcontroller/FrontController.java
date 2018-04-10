8bfb1ef143199ebfa53b1a2c9f121d9a73d7b893
beny <jlbeny100@gmail.com>
2018-04-11 00:26:37 +0200

    add servlet for *.do extension

---
+++ src/com/valoyes/patterns/frontcontroller/FrontController.java
@@ -0,0 +1,27 @@
+package com.valoyes.patterns.frontcontroller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Este Servlet gestionar'a cualquier request que llegue con la extension
+ * *.do
+ * @author benito
+ *
+ */
+@WebServlet("/*.do")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
